97a38a12af8145dc953cac9302e30771602e8a5e
EngRazanBalatiah <eng.razanBalatiah96@gmail.com>
2026-01-05 21:16:29 +0200

    First steps in Final Project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginToTrello',()=>{
+      // eng.razanbalatiah96@gmail.com
+   // qa!@#$%^&*
+   cy.visit("/login")
+   cy.fixture("trelloUser").then((data)=>{
+        cy.findByTestId("username").type(data.email)
+        cy.findByTestId("login-submit-idf-testid").click()
+        cy.findByTestId("password").type(data.password)
+        cy.findByTestId("login-submit-idf-testid").click()
+   })
+})
+
+Cypress.Commands.add('findByTestId',(dataTestId)=>{
+    cy.get(`[data-testid=${dataTestId}]`)
+})
